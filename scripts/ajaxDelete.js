e74e02fff906f6138daa9f9ec574bc9aa617a78e
Luís Ferreira <luisraferreira@gmail.com>
2019-03-18 10:38:53 +0000

    upd ajaxdelete

--- scripts/ajaxDelete.js
+++ scripts/ajaxDelete.js
@@ -1,33 +1,33 @@
-$.ajax({
-  type: 'GET',
-  url: 'https://jsonplaceholder.typicode.com/users',
-  success: function(data) {
+getUsers();
 
-    data.forEach(function(item) {
-      var name = item.name,
-        id = item.id,
-        username = item.username,
-        email = item.email,
-        website = item.website;
-
-
+function getUsers() {
+  $.ajax({
+    type: 'GET',
+    url: 'https://jsonplaceholder.typicode.com/users',
+    success: function(data) {
+      data.forEach(function(item) {
+        var name = item.name,
+          id = item.id,
+          username = item.username,
+          email = item.email,
+          website = item.website;
         var template = `<div class="user" data-id="${id}">
-                          <p class="name">Nome: ${name}</p>
-                          <p class="username">Username: ${username}</p>
-                          <p class="email">Email: ${email}</p>
-                          <p class="website">Website: ${website}</p>
+                            <p class="name">Nome: ${name}</p>
+                            <p class="username">Username: ${username}</p>
+                            <p class="email">Email: ${email}</p>
+                            <p class="website">Website: ${website}</p>
 
-                          <button class="deleteUser">Delete User</button>
-                        </div>`;
+                            <button class="deleteUser">Delete User</button>
+                          </div>`;
         console.log(name);
-
         $('.usersList').append(template);
-    });
+      });
 
-
-  },
-  error: function(xhr, type, exception) {
-    // if ajax fails display error alert
-    alert("ajax error response type " + type);
-  }
-});
+      //$('.deleteUser').on('click', userDelete);
+    },
+    error: function(xhr, type, exception) {
+      // if ajax fails display error alert
+      alert("ajax error response type " + type);
+    }
+  });
+}
